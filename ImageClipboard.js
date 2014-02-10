3e0a6aecede4c34f67d0f12b078fff04be984035
jorgenbs <jorgenbs@gmail.com>
2014-02-10 09:48:45 +0100

    broke it

--- ImageClipboard.js
+++ ImageClipboard.js
@@ -1,117 +1,97 @@
 /*jshint boss:true, laxcomma: true*/
 
-window.ImageClipboard = function (selector, callback) {
+!function (name, definition) {
+    if (typeof module != 'undefined') module.exports = definition
+    else if (typeof define == 'function' && define.amd) define(name, definition)
+    else this[name] = definition
+}('ImageClipboard', function (selector) {
   /*
     based on
     http://joelb.me/blog/2011/code-snippet-accessing-clipboard-images-with-javascript/  
   */
-  var el = document.querySelector(selector)
-    , pasteCatcher
-    , clipImage
-    , self = this;
-  
-  //pasting not supported, make pastecatcher
-  if (!window.Clipboard) {
-    pasteCatcher = _makePasteCatcher();
-  }
 
-  window.addEventListener('paste', pasteHandler);
+  var instance;
 
-  function pasteHandler (e) {
-    var items;
+  function init(selector, callback) {
+    this.prototype.el = document.querySelector(selector)
+    this.prototype.pasteCatcher = null
+    this.prototype.clipImage = null;
 
-    if (e.clipboardData && e.clipboardData.items) {
-      items = e.clipboardData.items;
-      
-      if (items) {
-        items = Array.prototype.filter.call(items, function (element) {
-          return element.type.indexOf("image") >= 0;
-        });
+    var callback = callback || null;
 
-        Array.prototype.forEach.call(items, function (item) {
-          var blob = item.getAsFile();
-          var urlObj = window.URL || window.webkitURL;
-          var source = urlObj.createObjectURL(blob);
+    function pasteHandler (e) {
+      var items;
 
-          loadImage(source);
-        });
+      if (e.clipboardData && e.clipboardData.items) {
+        items = e.clipboardData.items;
+        
+        if (items) {
+          items = Array.prototype.filter.call(items, function (element) {
+            return element.type.indexOf("image") >= 0;
+          });
+
+          Array.prototype.forEach.call(items, function (item) {
+            var blob = item.getAsFile();
+            var urlObj = window.URL || window.webkitURL;
+            var source = urlObj.createObjectURL(blob);
+
+            loadImage(source);
+          });
+        }
+      }
+      else if (pasteCatcher) {
+        //no direct access to clipboardData (firefox)
+        //use the pastecatcher
+        setTimeout(function () {
+        
+          var child = pasteCatcher.firstElementChild;
+
+          if (child && child.tagName == "IMG") {
+            loadImage(child.src);
+          }
+
+        }, 5); 
       }
     }
-    else if (pasteCatcher) {
-      //no direct access to clipboardData (firefox)
-      //use the pastecatcher
-      setTimeout(function () {
-      
-        var child = pasteCatcher.firstElementChild;
 
-        if (child && child.tagName == "IMG") {
-          loadImage(child.src);
+    function loadImage (source) {
+      var img = new Image();
+      this.prototype.el.innerHTML = "";
+
+      img.onload = function () {
+        //got picture, display it
+        var imgContainer = document.createElement("img");
+        imgContainer.src = img.src;
+        imgContainer.style.maxHeight = "100%";
+        imgContainer.style.maxHeight = "100%";
+        this.prototype.el.appendChild(imgContainer);
+
+        //empty out the ol' pastecatcher
+        if (pasteCatcher) pasteCatcher.innerHTML = "";
+
+        this.prototype.clipImage = img; 
+      };
+
+      img.src = src;
+    }
+
+    function getBase64 (img) {
+      if (img !== null) {
+        if (img.src.indexOf("blob://") !== -1) return img.src;
+
+        //convert blob to base64
+        var fr = new FileReader();
+        fr.onload = function (event) {
+          return event.target.result;
         }
-
-      }, 5); 
+        fr.readAsDataURL(img.src);
+      }
     }
-  }
 
-  var loadImage = function (src) {
-    var img = new Image();
-    el.innerHTML = "";
-
-    img.onload = function () {
-      //got picture, embed it
-      var imgContainer = document.createElement("img");
-      imgContainer.src = img.src;
-      imgContainer.style.maxHeight = "100%";
-      imgContainer.style.maxHeight = "100%";
-      el.appendChild(imgContainer);
-
-      if (pasteCatcher) pasteCatcher.innerHTML = "";
-
-      clipImage = img;
-
-      if (callback) callback(self);
-    };
-
-    img.src = src;
-  };
-
-  var getImage = function(){
-    return clipImage;
-  };
-
-  var destroy = function() {
-    var child;
-    
-    while (child = el.lastChild) {
-      el.removeChild(child);
+    return {
+      getBase64: getBase64,
     }
-    
-    if (pasteCatcher) {
-      while (child = pasteCatcher.lastChild)
-        pasteCatcher.removeChild(child);
-      document.body.removeChild(pasteCatcher);
-    }
-    
-    clipImage = null;
-  };
+  }  
 
-  function _makePasteCatcher() {
-    var pasteBox = document.createElement("div");
-
-    pasteBox.setAttribute("id", "paste_catcher");
-    pasteBox.setAttribute("contenteditable", "");
-    pasteBox.style.opacity = 0;
-    
-    document.body.appendChild(pasteBox);
-
-    pasteBox.focus();
-    document.addEventListener("click", function() { pasteBox.focus(); });
-
-    return pasteBox;
-  }
-
-  return {
-    el: el,
-    getImage: getImage,
-    destroy: destroy
-  };
-};
+  return instance || (instance = init(selector));
+});
